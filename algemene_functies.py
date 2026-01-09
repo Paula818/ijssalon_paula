38999564b2b9f504bf81a1cfb8c151afb41b1a70
Paula Achterhof <paulaachterhof@yahoo.com>
2026-01-09 16:27:00 +0100

    stavaza na huiswerk les 8

---
+++ algemene_functies.py
@@ -0,0 +1,7 @@
+def mijn_functie(mijn_int):
+    return mijn_int ** 2
+print (mijn_functie(27))
+
+def mijn_functie_2(getal1, getal2):
+    return [getal1 + getal2, getal1 - getal2, getal1 * getal2, getal1 // getal2]
+print (mijn_functie_2(12,2))
